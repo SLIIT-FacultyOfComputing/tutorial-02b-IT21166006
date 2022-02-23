4f32c06ab67a1a7792a9013534add4efb7a3cd53
Y.T.Dilshan <99633365+IT21166006@users.noreply.github.com>
2022-02-23 09:11:47 +0000

    I complete Tutorial 03.

--- tute02.cpp
+++ tute02.cpp
@@ -12,7 +12,7 @@ Type = 3
 OtRate = 1700
 Please Note that the input command in C++ is std::cin. This is a representation of the Keyboard.*/
 
-#include <stdio.h>
+/*#include <stdio.h>
 int main()
 {
    double salary, netSalary;
@@ -41,4 +41,37 @@ int main()
    printf("Net Salary is %f ", netSalary);
   
    return 0;
+}*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+  double salary, netSalary; //variable declaration 
+  int etype, otHrs, otRate;
+
+  cout<<"Enter Employee Type:"; //get inputs emp.type
+  cin >> etype;
+  
+  cout<< "Enter Salary:";  //get inputs salary
+  cin >> salary;
+  
+  cout<<"Enter OtHrs : ";  //get inputs othours
+  cin >>otHrs; 
+
+  switch (etype) {  //if condition
+      case 1 :
+          otRate = 1000;
+          break;
+      case 2 :
+          otRate = 1500;
+          break;
+      default :
+          otRate = 1700;
+          break;
+   }
+  netSalary = salary + otHrs* otRate;  //calculation
+  cout << "Net Salary is:"  << netSalary;  //output
+
+  return 0;
 }
--- tute04.cpp
+++ tute04.cpp
@@ -2,7 +2,7 @@
 
 Write a program to calculate the function called nCr which is defined as
 
-nCr = n!/ r!(n−r)!
+nCr = n!/ r!(n-r)!
 Where n! is the factorial of n.
 
 Implement the functions
@@ -13,17 +13,33 @@ Do not modify the main function.*/
 
 #include <iostream>
 
-long Factorial(int no);
+long Factorial(int no);  //variable declation
 long nCr(int n, int r);
 
 int main() {
   int n, r;
-  std::cout << "Enter a value for n ";
+  std::cout << "Enter a value for n "; //get inputs from users
   std::cin >> n;
   std::cout << "Enter a value for r ";
   std::cin >> r;
   std::cout << "nCr = ";
   std::cout << nCr(n,r);
   std::cout << std::endl;
+  
   return 0;
 }
+long Factorial(int no)  //factorial find function
+{
+    int fac=1;
+    for (int i=no;i>=1;i--)
+      {
+        fac=i*fac;
+      }
+      return fac;
+}
+long nCr(int n, int r)  //nCr find function
+{
+return Factorial(n) / (Factorial(r)*Factorial(n-r));
+  
+}
+
